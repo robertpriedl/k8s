93d6ff31134a0399d333f3296a3164975e1da83c
robertpriedl <robert.priedl@hotmail.com>
2022-08-06 13:06:58 +0200

    re-init

--- install-k8scluster-node1.sh
+++ install-k8scluster-node1.sh
@@ -1,5 +1,4 @@
-sudo apt install docker.io
-
+sudo apt install docker.io --assume-yes
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -10,20 +9,18 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
   "storage-driver": "overlay2"
 }
 EOF
-
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 apt update && apt upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl
-
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl -y
 sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
